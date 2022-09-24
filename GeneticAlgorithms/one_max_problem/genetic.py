1ca58956a4a46e5104626f8075141ba93e97d920
hanchiao <ye_hanqiao@163.com>
2022-09-25 00:13:30 +0800

    Hw3: One-Max Project Final version by Sep.25

--- GeneticAlgorithms/one_max_problem/genetic.py
+++ GeneticAlgorithms/one_max_problem/genetic.py
@@ -1,9 +1,9 @@
 # File: genetic.py
-#    from chapter 2 of _Genetic Algorithms with Python_
-
+# from chapter 2 of _Genetic Algorithms with Python_
 import random
 import statistics
 from itertools import count
+from enum import Enum
 import sys
 import time
 
@@ -12,10 +12,10 @@ def _generate_parent(length, geneSet, get_fitness):
     genes = []
     while len(genes) < length:
         sampleSize = min(length - len(genes), len(geneSet))
-        genes.extend(random.sample(geneSet, sampleSize))
+        genes.extend(random.choices(geneSet, k=sampleSize))
 
     fitness = get_fitness(genes)
-    return Chromosome(genes, fitness)
+    return Chromosome(genes, fitness, Strategies.Create)
 
 
 def _mutate(parent, geneSet, get_fitness):
@@ -26,35 +26,124 @@ def _mutate(parent, geneSet, get_fitness):
         if newGene != childGenes[index] \
         else alternative
     fitness = get_fitness(childGenes)
-    return Chromosome(childGenes, fitness)
+    return Chromosome(childGenes, fitness, Strategies.Mutate)
 
 
-def get_best(get_fitness, targetLen, optimalFitness, geneSet, display, iteration=500):
-    random.seed()
-    bestParent = _generate_parent(targetLen, geneSet, get_fitness)
-    display(bestParent)
-    if bestParent.Fitness >= optimalFitness:
-        return bestParent
+def _mutate_custom(parent, custom_mutate, get_fitness):
+    childGenes = custom_mutate(parent.Genes)
+    fitness = get_fitness(childGenes)
+    return Chromosome(childGenes, fitness, Strategies.Mutate)
 
-    cnt = count(0)
-    while next(cnt) < iteration:
-        child = _mutate(bestParent, geneSet, get_fitness)
 
-        if bestParent.Fitness >= child.Fitness:
-            continue
-        display(child)
-        if child.Fitness >= optimalFitness:
-            return child
+def _crossover(parent_1, parent_2, get_fitness, crossover):
+    childGenes = crossover(parent_1.Genes, parent_2.Genes)
+    fitness = get_fitness(childGenes)
+    return Chromosome(childGenes, fitness, Strategies.Crossover)
 
-        bestParent = child
 
-    return bestParent
+def get_best(get_fitness, targetLen, optimalFitness, geneSet,
+             display, custom_mutate=None, crossover=None, custom_create=None,
+             poolSize=1, crossover_rate=0.9,
+             generation=None):
+    if custom_mutate is None:
+        def fnMutate(parent):
+            return _mutate(parent, geneSet, get_fitness)
+    else:
+        def fnMutate(parent):
+            return _mutate_custom(parent, custom_mutate, get_fitness)
+
+    if custom_create is None:
+        def fnGenerateParent():
+            return _generate_parent(targetLen, geneSet, get_fitness)
+    else:
+        def fnGenerateParent():
+            genes = custom_create()
+            return Chromosome(genes, get_fitness(genes), Strategies.Create)
+
+    strategyLookup = {
+        Strategies.Create: lambda: fnGenerateParent(),
+        Strategies.Mutate: lambda p: fnMutate(p),
+        Strategies.Crossover: lambda p1, p2:
+        _crossover(p1, p2, get_fitness, crossover)
+    }
+    usedStrategies = []
+
+    for improvement in _get_improvement(strategyLookup, poolSize,
+                                        crossover_rate, generation):
+        display(improvement)
+        f = strategyLookup[improvement.Strategy]
+        usedStrategies.append(f)
+        if not optimalFitness > improvement.Fitness:
+            return improvement
+
+
+def _get_improvement(strategyLookup, poolSize, crossover_rate, iteration):
+    best = strategyLookup[Strategies.Create]()
+    yield best
+    population = [best]
+    historicalFitnesses = [best.Fitness]
+    for _ in range(poolSize - 1):
+        indiv = strategyLookup[Strategies.Create]()
+        if indiv.Fitness > best.Fitness:
+            best = indiv
+            yield best
+            historicalFitnesses.append(best.Fitness)
+
+        population.append(indiv)
+
+    generation = count(0)
+    while True:
+        if iteration is not None:
+            if next(generation) > iteration - 1:
+                break
+
+        w = [population[i].Fitness for i in range(poolSize)]
+        w_min = min(w)
+        w = [w[i] - w_min + 1 for i in range(poolSize)]  # scaling
+
+        for i in range(poolSize):
+            # crossover
+            if random.uniform(0, 1) < crossover_rate:
+                while True:
+                    [p1, p2, *_] = random.choices(list(range(poolSize)), weights=w, k=2)  # roulette wheel selection
+                    if not p1 == p2:
+                        break
+                child = strategyLookup[Strategies.Crossover](population[p1], population[p2])
+                population.append(child)
+                if child.Fitness > best.Fitness:
+                    best = child
+                    yield best
+                    historicalFitnesses.append(best.Fitness)
+
+            # mutation
+            child = strategyLookup[Strategies.Mutate](population[i])
+            if child.Fitness > best.Fitness:
+                best = child
+                yield best
+                historicalFitnesses.append(best.Fitness)
+            population[i] = child
+
+        # update population(selection)
+        size = len(population)
+        w = [population[i].Fitness for i in range(size)]
+        w_min = min(w)
+        w = [w[i] - w_min + 1 for i in range(size)]  # scaling
+
+        population = random.choices(population, weights=w, k=poolSize)
 
 
 class Chromosome:
-    def __init__(self, genes, fitness):
+    def __init__(self, genes, fitness, strategy):
         self.Genes = genes
         self.Fitness = fitness
+        self.Strategy = strategy
+
+
+class Strategies(Enum):
+    Create = 0,
+    Mutate = 1,
+    Crossover = 2,
+    Select = 3
 
 
 class Benchmark:
@@ -79,4 +168,3 @@ class Benchmark:
                     1 + i, mean,
                     statistics.stdev(timings, mean)
                     if i > 1 else 0))
-
--- GeneticAlgorithms/one_max_problem/oneMaxTests.py
+++ GeneticAlgorithms/one_max_problem/oneMaxTests.py
@@ -4,28 +4,66 @@
 import datetime
 import random
 import unittest
-
 import genetic
 
 
 def display(candidate, startTime):
     timeDiff = datetime.datetime.now() - startTime
     length = len(candidate.Genes)
-    print("{}...{}\t{:3.2f}\t{}".format(
-        ''.join(map(str, candidate.Genes if length < 25 else candidate.Genes[:10])),
-        ''.join(map(str, candidate.Genes if length < 25 else candidate.Genes[-10:])),
-        candidate.Fitness,
-        timeDiff))
+    if length < 25:
+        print("{}\t{:^6}\t{:<10}\t{}\n".format(
+            ' '.join(map(str, candidate.Genes)),
+            candidate.Fitness,
+            candidate.Strategy.name,
+            timeDiff))
+    else:
+        print("{}...{}\t{:^6}\t{:<10}\t{}\n".format(
+            ' '.join(map(str, candidate.Genes[:5])),
+            ' '.join(map(str, candidate.Genes[-5:])),
+            candidate.Fitness,
+            candidate.Strategy.name,
+            timeDiff))
+
 
 
 def get_fitness(genes):
     return genes.count(1)
 
 
+def crossover(parentGenes, donorGenes):
+    length = len(parentGenes)
+    cutIndex = random.randrange(1, length)
+    childGenes = parentGenes[:cutIndex] + donorGenes[cutIndex:]
+    return childGenes
+
+
+def mutate(genes, geneSet, mutate_rate):
+    childGenes = genes[:]
+
+    '''Two different definitions of MUTATE_RATE'''
+
+    # if random.uniform(0, 1) < mutate_rate:
+    #     index = random.randrange(0, len(genes))
+    #     newGene, alternative = random.sample(geneSet, 2)
+    #     childGenes[index] = newGene \
+    #         if newGene != childGenes[index] \
+    #         else alternative
+    # return childGenes
+
+    for i in range(len(genes)):
+        if random.uniform(0, 1) < mutate_rate:
+            newGene, alternative = random.sample(geneSet, 2)
+            childGenes[i] = newGene \
+                if newGene != childGenes[i] \
+                else alternative
+    return childGenes
+
+
 class OneMaxTests(unittest.TestCase):
     geneSet = [0, 1]
 
-    def test(self, length=100):
+    def solve(self, length=100, mutate_rate=0.01, crossover_rate=0.9,
+              generation=None, poolSize=20):
         startTime = datetime.datetime.now()
 
         def fnDisplay(candidate):
@@ -34,10 +72,24 @@ class OneMaxTests(unittest.TestCase):
         def fnGetFitness(genes):
             return get_fitness(genes)
 
+        def fnMutate(genes):
+            return mutate(genes, self.geneSet, mutate_rate)
+
+        def fnCrossover(parentGenes, donorGenes):
+            return crossover(parentGenes, donorGenes)
+
         optimalFitness = length
-        best = genetic.get_best(fnGetFitness, length, optimalFitness, self.geneSet, fnDisplay, iteration=1e5)
+        best = genetic.get_best(fnGetFitness, length, optimalFitness, self.geneSet,
+                                fnDisplay, crossover=fnCrossover, custom_mutate=fnMutate,
+                                poolSize=poolSize, crossover_rate=crossover_rate,
+                                generation=generation)
         self.assertEqual(best.Fitness, optimalFitness)
 
     def test_benchmark(self):
-        genetic.Benchmark.run(lambda: self.test(4000))
+        genetic.Benchmark.run(lambda: self.solve(length=30, mutate_rate=0.01,
+                                                 crossover_rate=0.9, generation=100,
+                                                 poolSize=20))
 
+
+if __name__ == '__main__':
+    unittest.main()
