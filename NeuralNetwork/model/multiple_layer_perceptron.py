ad69a42602174179a05a409f53d912cbcc037ae2
hanchiao <ye_hanqiao@163.com>
2022-11-19 22:00:37 +0800

    add activation layer custom feature

--- NeuralNetwork/model/multiple_layer_perceptron.py
+++ NeuralNetwork/model/multiple_layer_perceptron.py
@@ -8,11 +8,12 @@ from . import nnlayers
 
 
 class get_model:
-    def __init__(self, _input_dim, _output_dim, _num_hidden):
+    def __init__(self, _input_dim, _output_dim, _num_hidden, act_layer=nnlayers.Sigmoid(),
+                 output_layer=nnlayers.ActLinear()):
         self.linear1 = nnlayers.Linear(_input_dim, _num_hidden)
         self.linear2 = nnlayers.Linear(_num_hidden, _output_dim)
-        self.sigmoid = nnlayers.Sigmoid()
-        self.actlinear = nnlayers.ActLinear()
+        self.activation1 = act_layer
+        self.output = output_layer
 
         self.z_2 = None
         self.net_2 = None
@@ -23,18 +24,18 @@ class get_model:
     def forward(self, _x):
         self.x = _x
         self.net_1 = self.linear1(self.x)
-        self.z_1 = self.sigmoid(self.net_1)
+        self.z_1 = self.activation1(self.net_1)
         self.net_2 = self.linear2(self.z_1)
-        self.z_2 = self.actlinear(self.net_2)
+        self.z_2 = self.output(self.net_2)
 
         y = self.z_2.reshape(self.z_2.shape[0], -1)
 
         return y
 
     def backward(self, _error):
-        delta_1 = self.actlinear.backward(self.net_2, _error)
+        delta_1 = self.output.backward(self.net_2, _error)
         delta_2 = self.linear2.backward(self.z_1, delta_1)
-        delta_3 = self.sigmoid.backward(self.net_1, delta_2)
+        delta_3 = self.activation1.backward(self.net_1, delta_2)
         _ = self.linear1.backward(self.x, delta_3)
 
     def update(self, _lr):
